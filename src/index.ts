3752685d90d2a2321dc4feb629a657597a62aea7
AleksandraCyp <73715885+AleksandraCyp@users.noreply.github.com>
2021-01-31 11:06:02 +0100

    Comment out the script that makes the choose pokemon page visible

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-const newTemplate = document.querySelector('#choose-page-template')!;
-const app = document.querySelector('#app')!;
-app.innerHTML = newTemplate.innerHTML
+// Uncomment to see the choose pokemon page
+//const newTemplate = document.querySelector('#choose-page-template')!;
+//const app = document.querySelector('#app')!;
+//app.innerHTML = newTemplate.innerHTML
