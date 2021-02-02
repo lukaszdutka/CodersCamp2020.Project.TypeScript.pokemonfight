b7e4bd6afcc4940a6bd527fcba16bcf4fa13f887
AgataLudwiczynska <74932979+AgataLudwiczynska@users.noreply.github.com>
2021-02-02 20:32:53 +0100

    add new method
    + add activePokemon value
    + add alivePokemons array

--- src/playerClass.ts
+++ src/playerClass.ts
@@ -3,6 +3,8 @@ import { Pokemon } from "./pokemonClass";
 export class Player {
   private _name: string;
   private _pokemons: Pokemon[];
+  private _activePokemon: number;
+  private _alivePokemons: Pokemon[];
   constructor(name: string, pokemons: Pokemon[]) {
     if (name.length > 8) {
       throw new Error("Too long name");
@@ -12,11 +14,34 @@ export class Player {
     }
     this._name = name;
     this._pokemons = pokemons;
+    this._activePokemon = 0;
+    this._alivePokemons = pokemons;
   }
+
   get name(): string {
     return this._name;
   }
   get pokemons(): Pokemon[] {
     return this._pokemons;
+  }
+
+  set setActivePokemon(numOfActivePokemon: number) {
+    this._activePokemon = numOfActivePokemon;
+  }
+  get getActivePokemon(): number {
+    return this._activePokemon;
+  }
+
+  private isAlive() {
+    for (let i = 0; i < this._alivePokemons.length; i++) {
+      if (this._alivePokemons[i].isAlive() == false) {
+        this._alivePokemons.splice(i, 1);
+      }
+    }
+  }
+
+  get alivePokemons(): Pokemon[] {
+    this.isAlive();
+    return this._alivePokemons;
   }
 }
