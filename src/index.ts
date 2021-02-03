bb4ce0189333bb299eda106d3f18aab8a475c6ef
AleksandraCyp <73715885+AleksandraCyp@users.noreply.github.com>
2021-02-03 19:57:28 +0100

    merge with master

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// Uncomment to see the choose pokemon page
+//Uncomment to see the choose pokemon page
 //const newTemplate = document.querySelector('#choose-page-template')!;
 //const app = document.querySelector('#app')!;
 //app.innerHTML = newTemplate.innerHTML
