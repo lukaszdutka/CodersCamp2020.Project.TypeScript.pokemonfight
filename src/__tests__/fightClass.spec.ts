ec4e716f7371b2a3946a2130dcb8d5be034c56e1
Daria <44535712+daria305@users.noreply.github.com>
2021-02-04 10:32:41 +0100

    Mng 85 fight logic (#10)
    * Create fightClass
    
    * Add test for fight class
    
    * MNG-90 91 Fix tests and run the project with webpack server
    
    * MNG-85 Fix test error

---
+++ src/__tests__/fightClass.spec.ts
@@ -0,0 +1,22 @@
+import { Fight } from "../fightClass"
+
+
+const fight = new Fight();
+
+describe('Test fight class', () => {
+
+    it("Should return effectivenes of the attack for a given attack type and pokemon defending type", () => {
+        // Given
+        const attackTypes: string[] = ["normal", "electric", "fire"]
+        const pokemonTypes: string[][] = [["normal"], ["normal", "water"], ["bug", "grass"]]
+
+        // When
+        const effectiveness: number[] = [];
+        for (let i: number = 0; i < attackTypes.length; i++) {
+            effectiveness.push(fight.calculateAttackEffectiveness(attackTypes[i], pokemonTypes[i]))
+        }
+
+        // Then
+        expect(effectiveness).toEqual([1, 2, 4])
+    })
+})
---
+++ src/fightClass.ts
@@ -0,0 +1,39 @@
+export class Fight {
+
+    private readonly _fightChart =
+        [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.5, 0, 1, 1, 0.5, 1],
+        [1, 0.5, 0.5, 1, 2, 2, 1, 1, 1, 1, 1, 2, 0.5, 1, 0.5, 1, 2, 1],
+        [1, 2, 0.5, 1, 0.5, 1, 1, 1, 2, 1, 1, 1, 2, 1, 0.5, 1, 1, 1],
+        [1, 1, 2, 0.5, 0.5, 1, 1, 1, 0, 2, 1, 1, 1, 1, 0.5, 1, 1, 1],
+        [1, 0.5, 2, 1, 0.5, 1, 1, 0.5, 2, 0.5, 1, 0.5, 2, 1, 0.5, 1, 0.5, 1],
+        [1, 0.5, 0.5, 1, 2, 0.5, 1, 1, 2, 2, 1, 1, 1, 1, 2, 1, 0.5, 1],
+        [2, 1, 1, 1, 1, 2, 1, 0.5, 1, 0.5, 0.5, 0.5, 2, 0, 1, 2, 2, 0.5],
+        [1, 1, 1, 1, 2, 1, 1, 0.5, 0.5, 1, 1, 1, 0.5, 0.5, 1, 1, 0, 2],
+        [1, 2, 1, 2, 0.5, 1, 1, 2, 1, 0, 1, 0.5, 2, 1, 1, 1, 2, 1],
+        [1, 1, 1, 0.5, 2, 1, 2, 1, 1, 1, 1, 2, 0.5, 1, 1, 1, 0.5, 1],
+        [1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 0.5, 1, 1, 1, 1, 0, 0.5, 1],
+        [1, 0.5, 1, 1, 2, 1, 0.5, 0.5, 1, 0.5, 2, 1, 1, 0.5, 1, 2, 0.5, 0.5],
+        [1, 2, 1, 1, 1, 2, 0.5, 1, 0.5, 2, 1, 2, 1, 1, 1, 1, 0.5, 1],
+        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 0.5, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 0.5, 0],
+        [1, 1, 1, 1, 1, 1, 0.5, 1, 1, 1, 2, 1, 1, 2, 1, 0.5, 1, 0.5],
+        [1, 0.5, 0.5, 0.5, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 0.5, 2],
+        [1, 0.5, 1, 1, 1, 1, 2, 0.5, 1, 1, 1, 1, 1, 1, 2, 2, 0.5, 1]];
+
+    private readonly _typesForChart = ["normal", "fire", "water", "electric", "grass", "ice", "fighting", "poison", "ground", "flying", "psychic", "bug", "rock", "ghost", "dragon", "dark", "steel", "fairy"]
+
+    private _calculateDamage(attackType: string, pokemonDefenceType: string): number {
+        if (!this._typesForChart.includes(attackType)) {
+            throw new Error("Attack type does not exist")
+        } 
+        if (!this._typesForChart.includes(attackType)) {
+            throw new Error("Pokemon type does not exist")
+        }
+        return this._fightChart[this._typesForChart.indexOf(attackType)][this._typesForChart.indexOf(pokemonDefenceType)]
+    }
+
+    calculateAttackEffectiveness(attackType: string, pokemonDefenceTypes: string[]): number {
+        const damagePerType = pokemonDefenceTypes.map( (pokemonType: any): number => this._calculateDamage(attackType, pokemonType))
+        return damagePerType.reduce( (accumulator: number, value: number) => accumulator * value, 1)
+    }
+}
