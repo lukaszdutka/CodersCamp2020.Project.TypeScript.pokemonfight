491214480980c2b771e06fab2a8d36f0c27ae30f
mariusz-sm <mariusz.smarz@gmail.com>
2021-02-04 21:04:32 +0100

    Add keyup to input

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-//Uncomment to see the choose pokemon page
-//const newTemplate = document.querySelector('#choose-page-template')!;
-//const app = document.querySelector('#app')!;
-//app.innerHTML = newTemplate.innerHTML
+import { activateStart } from './activateStart';
+
+// Activate start button when both players set names
+const player1NameInput = document.querySelector('#enter-player1-name') as HTMLInputElement;
+const player2NameInput = document.querySelector('#enter-player2-name') as HTMLInputElement;
+const startButton = document.querySelector("#start-game-btn") as HTMLDivElement;
+
+player1NameInput.addEventListener("keyup", () => {
+    activateStart(player1NameInput, player2NameInput, startButton)
+})
+
+player2NameInput.addEventListener("keyup", () => {
+    activateStart(player1NameInput, player2NameInput, startButton)
+})
