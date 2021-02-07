6ae38f13651fb411d51997cb8a42d7ea31dff1e9
Daria Dziubaltowska <daria305@gmail.com>
2021-02-07 02:59:56 +0100

    MNG-84 Finish pokemon factory

--- src/pokemonFactory.ts
+++ src/pokemonFactory.ts
@@ -5,13 +5,36 @@ import { PokemonType } from "./pokemonClass"
 // defensePoints: number, types: PokemonType[], moves: { moveName: string; moveType: PokemonType }[]) 
 
 export class PokemonFactory {
-    readonly pokemons: PokemonType[];
+    readonly _pokemons: Pokemon[];
 
-    constructor(pokeData: [{name: string, types: PokemonType[], stats: {}, moves:[]}]) {
-        this.pokemons = [];
+    constructor(pokeData: {
+        name: string; types: string[];
+        stats: { hp: number; attack: number; defense: number; };
+        moves: { name: string; accuracy: number; power: number; power_points: number; type: string; }[]
+    }[]) {
+        this._pokemons = [];
 
         for (let pokemon of pokeData) {
-            //const pokemonObject = new Pokemon(pokemon.name, pokemon)
+            const pokeImg = "../assets/" + pokemon.name + ".png"
+            const pokeMoves = pokemon.moves.reduce( (tab, move) => {
+                tab.concat({moveName: move.name, moveType: move.type as PokemonType})
+                return tab
+                }, [] as {moveName: string, moveType: PokemonType}[])
+            const pokeTypes: PokemonType[] = pokemon.types.map(v => v as PokemonType)
+            
+            const pokemonObject = new Pokemon(pokemon.name, pokeImg, 
+                pokemon.stats.hp, pokemon.stats.attack, pokemon.stats.defense,
+                pokeTypes, pokeMoves)
+
+            this._pokemons.push(pokemonObject)
         }
+    }
+
+    get pokemons(): Pokemon[] {
+        return this._pokemons
+    }
+
+    getPokemonByName(name: string): Pokemon | undefined {
+        return this._pokemons.find( pokemon => pokemon.name === name)
     }
 }
