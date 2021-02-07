e6fec66ab97208f6a1007e8f911b99d85b772fb8
AleksandraCyp <73715885+AleksandraCyp@users.noreply.github.com>
2021-02-07 18:47:32 +0100

    Fix GameHandler class and add tests

--- src/gameHandlerClass.ts
+++ src/gameHandlerClass.ts
@@ -1,8 +1,10 @@
 import { Player } from "./playerClass";
-import { Pokemon } from "./pokemonClass";
 
 export class GameHandler {
-  constructor(private _playerOne: Player, private _playerTwo: Player) {}
+  constructor(
+    private readonly _playerOne: Player,
+    private readonly _playerTwo: Player
+  ) {}
 
   _currentPlayer: Player = this._playerOne;
 
@@ -15,28 +17,22 @@ export class GameHandler {
     return this._playerOne;
   }
 
+  private didPlayerLoose(player: Player): boolean {
+    return player.alivePokemons.length < 1;
+  }
+
   isGameFinished(): boolean {
-    const firstPlayerPokemons: Pokemon[] = this._playerOne.pokemons;
-    const secondPlayerPokemons: Pokemon[] = this._playerTwo.pokemons;
-    const allPokemons = firstPlayerPokemons.concat(secondPlayerPokemons);
-    const allHPs = allPokemons.map((pokemon) => pokemon._currentHP);
-    if (!allHPs.includes(0)) {
-      return false;
-    } else {
-      return true;
-    }
+    return (
+      this.didPlayerLoose(this._playerOne) ||
+      this.didPlayerLoose(this._playerTwo)
+    );
   }
 
   getWinner(): Player | Error {
     if (!this.isGameFinished())
-      return new Error("You cannot get the winner. The game is not over.");
-    const getallHPs = (player: Player): number[] =>
-      player.pokemons.map((pokemon) => pokemon._currentHP);
-    const firstPlayerPokemonsHP: number[] = getallHPs(this._playerOne);
-    if (firstPlayerPokemonsHP.includes(0)) {
-      return this._playerOne;
-    } else {
-      return this._playerTwo;
-    }
+      throw new Error("You cannot get the winner. The game is not over.");
+    return this.didPlayerLoose(this._playerOne) === true
+      ? this._playerTwo
+      : this._playerOne;
   }
 }
