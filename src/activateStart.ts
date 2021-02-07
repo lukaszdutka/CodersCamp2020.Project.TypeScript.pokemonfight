8ca1487d55dc74e7477c9d3b5abd0edb22288ea1
mariusz-sm <mariusz.smarz@gmail.com>
2021-02-07 20:44:07 +0100

    Add activateStartButton function

--- src/activateStart.ts
+++ src/activateStart.ts
@@ -1,6 +1,3 @@
-// import { config } from '../styles/SASS/config.scss'
-// Activate start button when both players set names
-
 export const activateStart = (name1Input: HTMLInputElement, name2Input: HTMLInputElement, startButton: HTMLDivElement) => {
     if (name1Input.value.length > 0 && name2Input.value.length > 0) {
         startButton.className = 'button'
