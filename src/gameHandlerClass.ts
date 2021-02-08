3a67f728dd358231ce5a54a260264aafafc96e58
AleksandraCyp <73715885+AleksandraCyp@users.noreply.github.com>
2021-02-08 08:11:31 +0100

    MNG-95 Add GameHandler class (#14)
    * Add GameHandler class
    
    * Fix GameHandler class and add tests
    
    * Fix tests

---
+++ src/gameHandlerClass.ts
@@ -0,0 +1,38 @@
+import { Player } from "./playerClass";
+
+export class GameHandler {
+  constructor(
+    private readonly _playerOne: Player,
+    private readonly _playerTwo: Player
+  ) {}
+
+  _currentPlayer: Player = this._playerOne;
+
+  getCurrentPlayer(): Player {
+    return this._currentPlayer;
+  }
+
+  getOpponentPlayer(): Player {
+    if (this._currentPlayer === this._playerOne) return this._playerTwo;
+    return this._playerOne;
+  }
+
+  private didPlayerLoose(player: Player): boolean {
+    return player.alivePokemons.length === 0;
+  }
+
+  isGameFinished(): boolean {
+    return (
+      this.didPlayerLoose(this._playerOne) ||
+      this.didPlayerLoose(this._playerTwo)
+    );
+  }
+
+  getWinner(): Player | Error {
+    if (!this.isGameFinished())
+      throw new Error("You cannot get the winner. The game is not over.");
+    return this.didPlayerLoose(this._playerOne) === true
+      ? this._playerTwo
+      : this._playerOne;
+  }
+}
