a866dd0d8f243d697931ff9272c5a14e355005b7
Gosia Dziewit <gosia.dziewit@gmail.com>
2021-02-10 11:09:37 +0100

    MNG-99 & MNG-100
    > created menu and back button with options.
    Options need to be fixed (now are default).
    Animation also needs to look better

--- src/gameHandlerClass.ts
+++ src/gameHandlerClass.ts
@@ -43,4 +43,67 @@ export class GameHandler {
       ? this._playerTwo
       : this._playerOne;
   }
+
+  generateActionButtons(player: Player): void {
+    console.log('action modal');
+    const actionContainer = document.querySelector("#actionModals")!;
+    actionContainer.innerHTML = `
+                    <div class="button battleButton" id="attackButton">attack</div>
+                    <div class="button battleButton" id="switchButton">switch</div>
+                    <div class="button battleButton" id="mangoButton">use mango (<span id="mango">1</span>)</div>`;
+    const battleButtons = document.getElementsByClassName("battleButton")! as HTMLCollectionOf<HTMLElement>;
+    const attackButton = document.querySelector("#attackButton")!;
+    const switchButton = document.querySelector("#switchButton")!;
+    //const mangoButton = document.querySelector("#mangoButton") as HTMLDivElement;
+    attackButton.addEventListener('click', () => {
+      this.generateAttackButtons(player);
+    });
+    switchButton.addEventListener('click', () => {
+      this.generateSwitchButtons(player);
+    });
+  }
+
+  generateAttackButtons(player: Player): void {
+    console.log('attack modal');
+    const actionContainer = document.querySelector("#actionModals")!;
+    actionContainer.innerHTML = `
+                    <div class="button battleButton" id="attackButtonOne">${player.pokemons[0].moves[0].moveName}</div>
+                    <div class="button battleButton" id="attackButtonTwo">${player.pokemons[0].moves[1].moveName}</div>
+                    <div class="button battleButton" id="backButton">back</div>`;
+    //const attackButtonOne = document.querySelector("#attackButtonOne") as HTMLDivElement;
+    //const attackButtonTwo = document.querySelector("#attackButtonTwo") as HTMLDivElement;
+    const battleButtons = document.getElementsByClassName("battleButton")! as HTMLCollectionOf<HTMLElement>;
+    const backButton = document.querySelector("#backButton")!;
+    backButton.addEventListener('click', () => {
+      //timeout and style for some button animation - the animation needs to be change
+      setTimeout( ()=> {
+        this.generateActionButtons(player);
+      }, 500);
+      for(let i = 0; i < battleButtons.length; i++) {
+        battleButtons[i].style.transform = 'scaleX(0)'; 
+      }
+    });
+  }
+
+  generateSwitchButtons(player: Player): void {
+    console.log('switch pokemon modal');
+    const actionContainer = document.querySelector("#actionModals")!;
+    actionContainer.innerHTML = `
+                    <div class="button battleButton" id="switchButtonOne">${player.pokemons[1].name}</div>
+                    <div class="button battleButton" id="switchButtonTwo">${player.pokemons[2].name}</div>
+                    <div class="button battleButton" id="backButton">back</div>`;
+    //const switchButtonOne = document.querySelector("#switchButtonOne") as HTMLDivElement;
+    //const switchButtonTwo = document.querySelector("#switchButtonTwo") as HTMLDivElement;
+    const battleButtons = document.getElementsByClassName("battleButton")! as HTMLCollectionOf<HTMLElement>;
+    const backButton = document.querySelector("#backButton")!;
+    backButton.addEventListener('click', () => {
+      //timeout and style for some button animation - the animation needs to be change
+      setTimeout( ()=> {
+        this.generateActionButtons(player);
+      }, 500);
+      for(let i = 0; i < battleButtons.length; i++) {
+        battleButtons[i].style.transform = 'scaleX(0)';
+      }
+    });
+  }
 }
