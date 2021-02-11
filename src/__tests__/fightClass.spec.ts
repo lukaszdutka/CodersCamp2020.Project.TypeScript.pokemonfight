54957de074c273576e1262f0bd04b154e604a01a
Daria Dziubaltowska <daria305@gmail.com>
2021-02-11 23:15:49 +0100

    Update test parametrization and do not use json data

--- src/__tests__/fightClass.spec.ts
+++ src/__tests__/fightClass.spec.ts
@@ -1,50 +1,43 @@
 import { Fight } from "../fightClass"
-import { PokemonType } from "../pokemonClass"
-import { PokemonFactory } from "../pokemonFactory"
-import * as data from "../../assets/poke_data.json"
+import { Pokemon, PokemonMove, PokemonType } from "../pokemonClass"
 
-const fight = new Fight();
 
 describe('Test fight class', () => {
+    const fight = new Fight();
+    type TestEffectivenessTuple = [PokemonType, PokemonType[], number];
 
-    it("Should return effectivenes of the attack for a given attack type and pokemon defending type", () => {
-        // Given
-        const attackTypes: PokemonType[] = ["normal", "electric", "fire"]
-        const pokemonTypes: PokemonType[][] = [["normal"], ["normal", "water"], ["bug", "grass"]]
+    it.each<TestEffectivenessTuple>([["normal", ["normal"], 1], ["electric", ["normal", "water"], 2], ["fire", ["bug", "grass"], 4]])
 
-        // When
-        const effectiveness: number[] = [];
-        for (let i: number = 0; i < attackTypes.length; i++) {
-            effectiveness.push(fight.calculateAttackEffectiveness(attackTypes[i], pokemonTypes[i]))
-        }
+        ("Should return effectivenes of the attack for a given attack type and pokemon defending type",
+            (attackType: PokemonType, defenceTypes: PokemonType[], expectedEffectiveness: number) => {
 
-        // Then
-        expect(effectiveness).toEqual([1, 2, 4])
-    })
+                expect(fight.calculateAttackEffectiveness(attackType, defenceTypes)).toBe(expectedEffectiveness)
+            })
 
-    it("Should return damage points and reduce pokemon hp points", () => {
-        // Given
-        const factory = new PokemonFactory(data.pokemons);
-        const rattata = factory.getPokemonByName("rattata");
-        const abra = factory.getPokemonByName("abra");
-        const fight = new Fight();
+    type TestFactoryTuple = [Pokemon, Pokemon, PokemonMove, number, number]
 
-        // When
-        const rattataDamge: number = fight.fight(abra, rattata, abra.moves[5]);
+    const pokemonNormal1 = new Pokemon("PokemonNormal", "poke1.jpg", 100, 30, 20, ["normal"], [{ moveName: "attackNormal", moveType: "normal" }])
+    const pokemonNormal2 = new Pokemon("PokemonNormal", "poke1.jpg", 100, 30, 20, ["normal"], [{ moveName: "attackNormal", moveType: "normal" }])
+    const pokemonGrass = new Pokemon("PokemonGrass", "poke2.jpg", 100, 30, 10, ["grass"], [{ moveName: "attackGrass", moveType: "grass" }])
+    const pokemonFire = new Pokemon("PokemonFire", "poke3.jpg", 100, 30, 10, ["fire"], [{ moveName: "attackFire", moveType: "fire" }])
 
-        // Then
-        expect(rattata.currentHP).toBe(rattata.maxHP - 10);
-        expect(rattataDamge).toBe(10);
+    const damage = (poke1: Pokemon, poke2: Pokemon, effect: number): number => Math.max(poke1.attackPoints - poke2.defensePoints, 5) * effect;
 
+    it.each<TestFactoryTuple>([
+        [pokemonNormal1, pokemonNormal2, pokemonNormal1.moves[0],
+            pokemonNormal2.currentHP - damage(pokemonNormal1, pokemonNormal2, 1), damage(pokemonNormal1, pokemonNormal2, 1)],
+        [pokemonGrass, pokemonFire, pokemonGrass.moves[0],
+            pokemonFire.currentHP - damage(pokemonGrass, pokemonFire, 1 / 2), damage(pokemonGrass, pokemonFire, 1 / 2)],
+        [pokemonFire, pokemonGrass, pokemonFire.moves[0],
+            pokemonGrass.currentHP - damage(pokemonFire, pokemonGrass, 2), damage(pokemonFire, pokemonGrass, 2)],
+    ])
 
-        // When
-        console.log( rattata.moves[2].moveType, abra.types);
-        console.log(fight.calculateAttackEffectiveness(rattata.moves[2].moveType, abra.types));
-        const abraDamage: number = fight.fight(rattata, abra, rattata.moves[2]);
-        
-        // Then
-        expect(abra.currentHP).toBe(0);
-        expect(abraDamage).toBe(82);
+        (`Should return damage points and reduce pokemon hp points`,
+            (attackPoke: Pokemon, defendingPoke: Pokemon, move: PokemonMove, HPLeft: number, damage: number) => {
 
-    })
+                const defendingDamage: number = fight.fight(attackPoke, defendingPoke, move);
+
+                expect(defendingPoke.currentHP).toBe(HPLeft);
+                expect(defendingDamage).toBe(damage);
+            })
 })
