b3e7d74401600fbd9f715c3b3264a53aad216912
AgataLudwiczynska <74932979+AgataLudwiczynska@users.noreply.github.com>
2021-02-11 16:54:49 +0100

    Blocking button when the name is too long

--- src/activateStart.ts
+++ src/activateStart.ts
@@ -1,5 +1,5 @@
 export const activateStart = (name1Input: HTMLInputElement, name2Input: HTMLInputElement, startButton: HTMLButtonElement) => {
-    if (name1Input.value.length > 0 && name2Input.value.length > 0) {
+    if (name1Input.value.length > 0 && name2Input.value.length > 0 && name1Input.value.length <= 8 && name2Input.value.length <= 8) {
         startButton.className = 'button-enabled'
         startButton.disabled = false;
     } else {
