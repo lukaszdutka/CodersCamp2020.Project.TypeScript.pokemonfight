2277b9b9adebe16cb89c44848c237d559c73a086
Gosia Dziewit <gosia.dziewit@gmail.com>
2021-02-12 09:02:32 +0100

    fixed underline animation for active player

--- src/fightPage/createActivePlayer.ts
+++ src/fightPage/createActivePlayer.ts
@@ -7,11 +7,21 @@ export const createActivePlayer = (gameHandler: GameHandler) => {
   activePlayerArea.textContent = `${gameHandler.currentPlayer.name} now is your turn. Get the revenge!`;
   const playerOneUnderlineArea = document.querySelector("#fight-page-player1");
   const playerTwoUnderlineArea = document.querySelector("#fight-page-player2");
+  const underlineOne = document.querySelector("#underline1") as HTMLDivElement;
+  const underlineTwo = document.querySelector("#underline2") as HTMLDivElement;
   if (gameHandler.currentPlayer === gameHandler.playerOne) {
-    playerTwoUnderlineArea?.classList.remove("activePlayer");
+    setTimeout(() => {
+     playerTwoUnderlineArea?.classList.remove("activePlayer");
+    }, 500);
     playerOneUnderlineArea?.classList.add("activePlayer");
+    underlineTwo.style.animation = "scale-out-horizontal 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both";
+    underlineOne.style.animation = "scale-in-hor-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
   } else {
-    playerOneUnderlineArea?.classList.remove("activePlayer");
+    setTimeout(() => {
+      playerOneUnderlineArea?.classList.remove("activePlayer");
+     }, 500);
     playerTwoUnderlineArea?.classList.add("activePlayer");
+    underlineOne.style.animation = "scale-out-horizontal 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both";
+    underlineTwo.style.animation = "scale-in-hor-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
   }
 };
