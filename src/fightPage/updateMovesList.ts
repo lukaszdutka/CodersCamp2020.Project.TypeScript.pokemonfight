243c24b4600173512f8b0902d9cd00cdc07ffe96
AleksandraCyp <73715885+AleksandraCyp@users.noreply.github.com>
2021-02-12 07:47:58 +0100

    MNG-113 Add game logs (#26)

---
+++ src/fightPage/updateMovesList.ts
@@ -0,0 +1,32 @@
+import { GameHandler } from "../gameHandlerClass";
+import { attack, switchPoke, eatMango } from "./buttonsEventListeners";
+
+export const updateMovesList = (
+  gameHandler: GameHandler,
+  calledFunction: any,
+  event: Event
+) => {
+  const moveList = document.querySelector(".gameLogs")!;
+  const newMove = document.createElement("p");
+  let newMoveText = document.createTextNode("No move found");
+  const pokemonName = gameHandler.currentPlayer.getActivePokemon.name;
+  const buttonContent = (event.currentTarget as HTMLDivElement).textContent;
+  const playerName = gameHandler.currentPlayer.name;
+  console.log();
+  switch (calledFunction) {
+    case attack:
+      newMoveText.nodeValue = `${playerName}: ${pokemonName} used ${buttonContent}!`;
+      break;
+    case switchPoke:
+      newMoveText.nodeValue = `${playerName} changed Pokemon: ${pokemonName} comes into play!`;
+      break;
+    case eatMango:
+      newMoveText.nodeValue = `${playerName} used mango: ${pokemonName} is healed!`;
+      break;
+    default:
+      console.log("No move found");
+  }
+  console.log(newMove);
+  newMove.append(newMoveText);
+  moveList.insertBefore(newMove, moveList.firstChild);
+};
