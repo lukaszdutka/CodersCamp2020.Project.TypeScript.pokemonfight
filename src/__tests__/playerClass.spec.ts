2eeeb1c71a9c36dc13fee51bd85c094445836cc8
mariusz-sm <mariusz.smarz@gmail.com>
2021-02-15 15:40:31 +0100

    Add test for addPokemon function

--- src/__tests__/playerClass.spec.ts
+++ src/__tests__/playerClass.spec.ts
@@ -1,4 +1,5 @@
 import { Player } from "../playerClass";
+import { Pokemon } from "../pokemonClass";
 import { PokemonFactory } from "../pokemonFactory";
 import * as data from "../../assets/poke_data.json";
 
@@ -6,19 +7,21 @@ describe("Test Player class", () => {
     const pokeData = data.pokemons;
     const factory = new PokemonFactory(pokeData);
 
+    const player = (name: string, pokemons: Pokemon[]) => {
+        return new Player(name, pokemons)
+    };
+
     it("Should throw error when player name is longer than 8", () => {
         // Given
         const name = "Elizabeth"
-        const player = (name: string) => {
-            return new Player(name, [
-                factory.getPokemonByName("bulbasaur"),
-                factory.getPokemonByName("charmander"),
-                factory.getPokemonByName("pikachu"),
-            ])
-        };
+        const pokemonList = [
+            factory.getPokemonByName("bulbasaur"),
+            factory.getPokemonByName("charmander"),
+            factory.getPokemonByName("pikachu"),
+        ]
     
         // When
-        const playerWithTooLongName = () => player(name)
+        const playerWithTooLongName = () => player(name, pokemonList)
     
         // Then
         expect(playerWithTooLongName).toThrow("Too long name")
@@ -26,17 +29,16 @@ describe("Test Player class", () => {
 
     it("Should throw error when list of pokemons is longer than 3", () => {
         // Given
-        const player = () => {
-            return new Player("Mark", [
-                factory.getPokemonByName("bulbasaur"),
-                factory.getPokemonByName("charmander"),
-                factory.getPokemonByName("pikachu"),
-                factory.getPokemonByName("squirtle")
-            ])
-        };
+        const name = "Mark"
+        const pokemonList = [
+            factory.getPokemonByName("bulbasaur"),
+            factory.getPokemonByName("charmander"),
+            factory.getPokemonByName("pikachu"),
+            factory.getPokemonByName("squirtle")
+        ]
     
         // When
-        const playerWithTooMuchPokemens = () => player()
+        const playerWithTooMuchPokemens = () => player(name, pokemonList)
     
         // Then
         expect(playerWithTooMuchPokemens).toThrow("Wrong number of pokemons")
@@ -44,16 +46,15 @@ describe("Test Player class", () => {
 
     it("Should throw error when player has not mango", () => {
         // Given
-        const player = () => {
-            return new Player("Mark", [
-                factory.getPokemonByName("bulbasaur"),
-                factory.getPokemonByName("charmander"),
-                factory.getPokemonByName("pikachu")
-            ])
-        };
+        const name = "Mark"
+        const pokemonList = [
+            factory.getPokemonByName("bulbasaur"),
+            factory.getPokemonByName("charmander"),
+            factory.getPokemonByName("pikachu"),
+        ]
     
         // When
-        const playerOne = player()
+        const playerOne = player(name, pokemonList)
         const useMango = () => playerOne.useMango()
         useMango()
     
@@ -61,8 +62,20 @@ describe("Test Player class", () => {
         expect(useMango).toThrow("You cannot use mango. You use all mangos")
     });
     
+    it("Checks if addPokemon function works", () => {
+        // Given
+        const name = "Mark"
+        const pokemonList = [
+            factory.getPokemonByName("bulbasaur"),
+            factory.getPokemonByName("charmander")
+        ]
     
-
-
-
+        // When
+        const playerOne = player(name, pokemonList)
+        playerOne.addPokemon(factory.getPokemonByName("squirtle"))
+        const numberOfPokemons = playerOne.pokemons.length
+    
+        // Then
+        expect(numberOfPokemons).toEqual(3)
+    });
 });
